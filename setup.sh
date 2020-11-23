7058665730120ada548035a996cf51b0bdff7f40
Eric <erichadley8@gmail.com>
2020-11-23 16:04:41 -0500

    fixed PATH

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ apt -y upgrade
 apt install python3-pip
 apt install -y build-essential libssl-dev libffi-dev python3-dev
 pip3 install -r requirements.txt
-export PATH=$PATH:$PWD/selenium-head-bot/
+export PATH=$PATH:$PWD
