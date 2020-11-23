b7340a3e757a85df03ef351bba8009838b05f5a3
Eric <erichadley8@gmail.com>
2020-11-23 13:44:25 -0500

    finished script

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from selenium import webdriver
+import random
 
-driver = webdriver.Firefox()
-driver.get('http://wwww.porngifs.xyz')
-driver.close()
+while True:
+
+	driver = webdriver.Firefox()
+	driver.get('http://www.porngifs.xyz/')
+
+	#handle=driver.window_handles
+	#print(handle)
+	driver.switch_to.window(driver.window_handles[1])
+	driver.close()
+
+	driver.switch_to.window(driver.window_handles[0])
+
+	elements = driver.find_elements_by_tag_name('iframe')
+
+	click = random.choice(elements)
+	if "jads" in click.get_attribute("src"): 
+		click.click()
+		driver.close()
+
+	driver.switch_to.window(driver.window_handles[0])
+	driver.close()
